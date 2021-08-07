41f967f8e0188c0eb8b0cf6a331cc35f971c4ed5
Andrew Popov <popov_ey@mail.ru>
2021-08-07 14:52:22 +0300

    Add upload img function to messages

--- src/main/java/com/example/servingwebcontent/controller/MainController.java
+++ src/main/java/com/example/servingwebcontent/controller/MainController.java
@@ -5,6 +5,7 @@ import com.example.servingwebcontent.domain.User;
 import com.example.servingwebcontent.repos.MessageRepo;
 import org.hibernate.criterion.NotNullExpression;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,14 +13,21 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/info")
     public String info(Map<String, Object> model) {
@@ -52,8 +60,24 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model) {
+            @RequestParam("file") MultipartFile file,
+            @RequestParam String tag, Map<String, Object> model
+    ) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if (file != null) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
 
         messageRepo.save(message);
 
